ba708a10c50bd999646df64b72c947e2776715d2
Emre <ct.edag@gmail.com>
2023-06-14 16:42:43 +0300

    exercise3Done

--- src/Exercisess/exercise-3-type-assertions-start.ts
+++ src/Exercisess/exercise-3-type-assertions-start.ts
@@ -1,23 +1,18 @@
-/**
- * START: Follow the instructions below.
- */
-
 function getPopulation(): any {
     return 69_950_850;
 }
 
 // Add a type assertion after the call to `getPopulation()`.
-// Hint: What's the actual type of the value returned by `getPopulation()`?
+// The actual type of the value returned by `getPopulation()` is `number`.
 
-const population = getPopulation();
+const population = getPopulation() as number;
 
 // Now you've added a type assertion, there's a type error in the code below.
 // Remove the code that is incorrect.
 
-console.log(population.toUpperCase());
+//console.log(population.toUpperCase());
 
 console.log(population * 2);
-
 // ----
 
 export {};
