713b75626c023e048c4b22985adf376cf96b8f40
Emre <ct.edag@gmail.com>
2023-06-23 14:48:54 +0300

    exercise22Done

--- src/Exercisess/exercise-2-construct-types-with-record-start.ts
+++ src/Exercisess/exercise-2-construct-types-with-record-start.ts
@@ -1,43 +1,46 @@
 /**
- * START: Follow the instructions below.
- */
 
-// Fix this use of the `Record` utility type so it constructs a valid type.
+START: Follow the instructions below.
+*/
+// Fix this use of the Record utility type so it constructs a valid type.
 
-const currency: Record<> = {
+const currency: Record<string, string> = {
     name: "United States dollar",
     code: "USD",
     symbol: "$",
-};
-
-// Add a type annotation to the `currencies` variable that uses
-// the `Record` utility type.
-// Hint: Pass the `Currency` type as the second type argument to `Record<>`.
-
-interface Currency {
+    };
+    
+    // Add a type annotation to the currencies variable that uses
+    // the Record utility type.
+    // Hint: Pass the Currency type as the second type argument to Record<>.
+    
+    interface Currency {
     name: string;
     code: string;
     symbol: string;
-}
-
-const currencies = {
+    }
+    
+    const currencies: Record<string, Currency> = {
     france: {
-        name: "Euro",
-        code: "EUR",
-        symbol: "€",
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
     },
     china: {
-        name: "Renminbi",
-        code: "CNY",
-        symbol: "¥",
+    name: "Renminbi",
+    code: "CNY",
+    symbol: "¥",
     },
     colombia: {
-        name: "Colombian peso",
-        code: "COP",
-        symbol: "$",
+    name: "Colombian peso",
+    code: "COP",
+    symbol: "$",
     }
-};
-
-// ----
-
-export {};
+    };
+    
+    // ----
+    
+    console.log(currency);
+    console.log(currencies);
+    
+    export {};
