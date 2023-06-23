cb66e16a918e2952ede7b5aeb0b2acec39d49873
Emre <ct.edag@gmail.com>
2023-06-23 13:11:19 +0300

    exercise10Done

--- src/Exercisess/exercise-2-make-an-interface-generic-start.ts
+++ src/Exercisess/exercise-2-make-an-interface-generic-start.ts
@@ -6,9 +6,9 @@
 // This should fix the type errors on Lines 14 and 21.
 // Hint: The type for the `languages` property should use a type variable.
 
-interface Country {
+interface Country<T> {
     name: string;
-    languages: unknown;
+    languages: T;
 }
 
 const languagesObj1: Country<string> = {
